af40b6f51a2809b473aa3dfdede0f65e6fd4685f
Afrish Khan S <ryanafrish7@gmail.com>
2019-04-03 02:16:30 +0530

    Add eslint and its configuration

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: "airbnb-base",
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+    },
+    parser: "babel-eslint",
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
+    },
+    rules: {
+        "quotes": ["error", "double"],
+        "no-underscore-dangle": ["error", { "allowAfterThis": true }],
+        "no-param-reassign": ["error", { "props": false }],
+        "class-methods-use-this": ["error", { "exceptMethods": ["renderStyle"] }],
+        "indent": ["error", 4, { "ignoredNodes": ["TemplateLiteral *"] }]
+    },
+};
